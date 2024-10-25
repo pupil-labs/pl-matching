168d67b2ec7ba6a58d3ef870f50e599c8592ab7f
Marc Tonsen <mar@pupil-labs.com>
2024-10-25 15:38:30 +0200

    Added test using all sensors

--- tests/test_neon.py
+++ tests/test_neon.py
@@ -1,19 +1,24 @@
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 import pupil_labs.neon_recording as nr
 from pupil_labs.matching.matcher import Matcher, MatchingMethod
 
 
+# create fixture for neon recording
+@pytest.fixture
+def rec():
+    rec_dir = Path("tests/data/demo_recording")
+    return nr.load(rec_dir)
+
+
 @pytest.mark.parametrize(
     "sensor_name",
     ["gaze", "eye_state", "events"],
 )
-def test_scalar_data(sensor_name: str):
-    rec_dir = Path("tests/data/demo_recording")
-    rec = nr.load(rec_dir)
-
+def test_scalar_data(rec: nr.NeonRecording, sensor_name: str):
     sensor = getattr(rec, sensor_name)
     target_ts = sensor.timestamps
     matcher = Matcher(target_ts, [sensor], MatchingMethod.NEAREST)
@@ -29,10 +34,7 @@ def test_scalar_data(sensor_name: str):
         "scene",
     ],
 )
-def test_video_data(sensor_name: str):
-    rec_dir = Path("tests/data/demo_recording")
-    rec = nr.load(rec_dir)
-
+def test_video_data(rec: nr.NeonRecording, sensor_name: str):
     sensor = getattr(rec, sensor_name)
     target_ts = sensor.timestamps
     matcher = Matcher(target_ts, [sensor], MatchingMethod.NEAREST)
@@ -41,13 +43,13 @@ def test_video_data(sensor_name: str):
         assert matcher[i][0].index == i
 
 
-def test_multi_rec():
-    rec_dir = Path("tests/data/demo_recording")
-    rec1 = nr.load(rec_dir)
-    rec1 = nr.load(rec_dir)
+def test_iteration(rec: nr.NeonRecording):
+    target_ts = np.arange(0, 10, 0.1) + rec.start_ts
+    matcher = Matcher(
+        target_ts,
+        [rec.gaze, rec.eye_state, rec.eye, rec.scene, rec.events],
+        MatchingMethod.NEAREST,
+    )
 
-    target_ts = rec1.gaze.timestamps
-    matcher = Matcher(target_ts, [rec1.gaze], MatchingMethod.NEAREST)
-
-    for i in range(len(matcher)):
-        assert matcher[i][0].ts == target_ts[i]
+    for _ in matcher:
+        pass
